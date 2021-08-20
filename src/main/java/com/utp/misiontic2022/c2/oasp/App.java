1d30e98be9d5029dea5b13365d7c76a2fadd916a
oralsepa <orlandosedano@yahoo.es>
2021-08-20 18:36:55 -0500

    inicial

---
+++ src/main/java/com/utp/misiontic2022/c2/oasp/App.java
@@ -0,0 +1,13 @@
+package com.utp.misiontic2022.c2.oasp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/utp/misiontic2022/c2/oasp/AppTest.java
@@ -0,0 +1,20 @@
+package com.utp.misiontic2022.c2.oasp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
